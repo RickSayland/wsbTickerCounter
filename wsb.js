1a94fbc707d30e21b556d9da6bdcbdb60948c634
rsayl <rsayl@RICKDESKTOP>
2020-02-23 12:31:17 -0500

    fixed so it goes one reply layer deep and also wont double count

--- wsb.js
+++ wsb.js
@@ -59,33 +59,6 @@ var wsb = {
             }
         });
     },
-    // getTickers: (url) => {
-    //     return $.ajax({
-    //         dataType: "json",
-    //         url: url,
-    //         success: (result) => {
-    //             var comments = result[1].data.children;
-    //             $.each(comments, (index) => {
-    //                 var comment = comments[index].data.body;
-    //                 if (comment) {
-    //                     var match = comment.match(tickerRegex);
-    //                     if (match && wsb.isValidTicker(match[0])) {
-    //                         //If we dont have it yet, add it to the list
-    //                         if ($.inArray(match[0],wsb.tickers) == -1) {
-    //                             listOfTickers.push(match[0]);
-    //                             wsb.tickers.push(match[0]);
-    //                         }
-    //                         //If we do have it, only update internal list
-    //                         if ($.inArray(match[0],wsb.tickers) >= 0) {
-    //                             wsb.tickers.push(match[0])
-    //                         }
-    //                     }
-    //                 };
-    
-    //             });
-    //         }
-    //     });
-    // },
     getTickerCounts: () => {
         var ticketCounts = [];
         $.each(listOfTickers,(index,value) => {
